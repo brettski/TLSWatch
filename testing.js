9231698ad1be2f8e01dfaa21a096895f32bd6e3f
Brett Slaski <brettski@yahoo.com>
2017-05-02 22:29:53 -0500

    mongo

--- testing.js
+++ testing.js
@@ -5,7 +5,6 @@ We can use tls module to get the certificate.  All the needed data is inthere, j
 http://stackoverflow.com/questions/14599901/read-expiration-date-and-common-name-from-ssl-certificate-using-node-js
 Loop through array: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
 
-
 require('tls');
 var cn = tls.connect(443, "www.surveysage.com");
 // the cert is in json form
@@ -16,7 +15,6 @@ for (var item in san) {
   san[item] = san[item].trim();
   san[item] = san[item].substr(4);
 }
-
 */
 
 const certf = require('./cert');
@@ -28,11 +26,53 @@ certf.getCert('tlswatch.com', function (err, cert) {
     return;
   }
 
-  const jsonCert = JSON.stringify(cert);
-  console.log('cert:\n', jsonCert.subject);
+  console.log('cert:\n', cert.subject);
+  //mongo(cert);
 });
-//test()
+
 return;
+
+
+function mongo(cert) {
+  var MongoClient = require('mongodb').MongoClient;
+  var assert = require('assert');
+  var mongovar = require('./mongodb.env');
+  var uri = mongovar.mongodb.uri;
+  //console.log('values:\n', uri);
+
+  var insertCertificate = function (db, callback) {
+    var collection = db.collection('certificate');
+    collection.insert(cert, function(err, result) {
+      assert.equal(err, null);
+      assert.equal(1, result.result.n);
+      assert.equal(1, result.ops.length);
+      console.log('Inserted the certificate into the database');
+      callback(result);
+    });
+  }
+
+  var findCertificate = function(db, callback) {
+    var collection = db.collection('certificate');
+    collection.find({}).toArray(function(err, certs) {
+      assert.equal(err, null);
+      console.log('Found the following certs');
+      console.log(certs);
+      callback(certs);
+    })
+  }
+
+  MongoClient.connect(uri, function(err, db) {
+    assert.equal(null, err);
+    console.log('Connected successfully to server');
+
+    insertCertificate(db, function() {
+      findCertificate(db, function() {
+        db.close();
+      });
+    });
+  });
+}
+
 
 
 function test() {
